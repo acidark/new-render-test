379a193bd84e7683d7600903ac8034c95e0d61b3
eoeo <eoeo@localhost.localdomain>
2024-10-17 13:16:12 +0300

    build

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const requestLogger = (request,response,next) => {
 }
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 app.use(requestLogger)
 let notes = [
